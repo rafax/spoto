3f62dd05ebb4486c008ecead403a46d579f18fae
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2015-04-04 21:44:32 +0200

    Working version deployable to ec2

--- server.go
+++ server.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/gocraft/web"
+	_ "github.com/lib/pq"
 	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	insert *sql.Stmt
+	db     *sql.DB
 )
 
 type Context struct {
@@ -12,13 +21,10 @@ type Context struct {
 
 type Notification struct {
 	SubscriptionId string `json:"subscription_id"`
-	Object         string `json:"object"`
 	ObjectId       string `json:"object_id"`
+	Object         string `json:"object"`
 	ChangedAspect  string `json:"changed_aspect"`
-}
-
-func (c *Context) SetHelloCount(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
-	next(rw, req)
+	TimeChanged    int64  `json:"time"`
 }
 
 func (c *Context) VerifyInstagram(rw web.ResponseWriter, req *web.Request) {
@@ -32,20 +38,46 @@ func (c *Context) VerifyInstagram(rw web.ResponseWriter, req *web.Request) {
 	fmt.Fprint(rw, chal)
 }
 
-func (c *Context) StoreNotifications(rw web.ResponseWriter, req *web.Request) {
+func (c *Context) Ping(rw web.ResponseWriter, req *web.Request) {
+	err := db.Ping()
+	checkErr(err)
+	fmt.Fprint(rw, "pong")
+}
+
+func (c *Context) ReceiveNotifications(rw web.ResponseWriter, req *web.Request) {
 	notifications := make([]Notification, 0)
 	decoder := json.NewDecoder(req.Body)
 	decoder.Decode(&notifications)
+	for _, n := range notifications {
+		err := insert.QueryRow(n.ObjectId, n.ObjectId, n.ChangedAspect, time.Unix(n.TimeChanged, 0)).Scan(&sql.NullInt64{})
+		checkErr(err)
+	}
 	fmt.Fprint(rw, notifications)
 }
 
 func main() {
-	router := web.New(Context{}). // Create your router
-					Middleware(web.LoggerMiddleware).             // Use some included middleware
-					Middleware(web.ShowErrorsMiddleware).         // ...
-					Middleware((*Context).SetHelloCount).         // Your own middleware!
-					Get("/insta", (*Context).VerifyInstagram).    // Add a route
-					Post("/insta", (*Context).StoreNotifications) // Add a route
-
+	dbhost := os.Getenv("SPOTO_DB_HOST")
+	if len(dbhost) == 0 {
+		dbhost = "localhost"
+	}
+	cs := fmt.Sprintf("user=spoto password=%s dbname=spoto sslmode=disable host=%s", "otops", dbhost)
+	fmt.Println(cs)
+	var err error
+	db, err = sql.Open("postgres", cs)
+	checkErr(err)
+	insert, err = db.Prepare("INSERT INTO \"notifications\" (iid, object, changed_aspect, changed_time) VALUES($1,$2,$3,$4) returning id;")
+	checkErr(err)
+	router := web.New(Context{}).
+		Middleware(web.LoggerMiddleware).
+		Middleware(web.ShowErrorsMiddleware).
+		Get("/insta", (*Context).VerifyInstagram).
+		Get("/ping", (*Context).Ping).
+		Post("/insta", (*Context).ReceiveNotifications)
 	http.ListenAndServe("localhost:3000", router)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
