f6412f9767b9557d05290c5f77488b306bfe4f1d
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2015-05-05 21:31:02 +0200

    Switch to ubuntu

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 GOOS=linux GOARCH=amd64 go build
 zip spoto.zip spoto
-scp -i /Users/rafal/dev/aws/rafal.pem -P 2225 /Users/rafal/dev/go/src/github.com/rafax/spoto/spoto.zip ec2-user@52.1.82.203:/app/spoto
-ssh -i /Users/rafal/dev/aws/rafal.pem ec2-user@52.1.82.203 -p 2225 'rm /app/spoto/spoto ; unzip /app/spoto/spoto.zip -d /app/spoto ; killall spoto ; SPOTO_DB_HOST=spoto.c0rawtfsbsk2.us-east-1.rds.amazonaws.com nohup /app/spoto/spoto > /dev/null 2>&1 &'
+scp -i /Users/rafal/dev/aws/rafal.pem -P 2225 /Users/rafal/dev/go/src/github.com/rafax/spoto/spoto.zip ubuntu@52.1.82.203:/app/spoto
+ssh -i /Users/rafal/dev/aws/rafal.pem ubuntu@52.1.82.203 -p 2225 'rm /app/spoto/spoto ; unzip /app/spoto/spoto.zip -d /app/spoto ; killall spoto ; SPOTO_DB_HOST=spoto.c0rawtfsbsk2.us-east-1.rds.amazonaws.com nohup /app/spoto/spoto | logger 2>&1 &'
