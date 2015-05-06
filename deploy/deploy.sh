1ad928efaabe77c9b260fa4092f6044081521a80
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2015-05-06 22:14:00 +0200

    Switch to tar

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 GOOS=linux GOARCH=amd64 go build
-zip spoto.zip spoto
-scp -i /Users/rafal/dev/aws/rafal.pem -P 2225 /Users/rafal/dev/go/src/github.com/rafax/spoto/spoto.zip ubuntu@52.1.82.203:/app/spoto
-ssh -i /Users/rafal/dev/aws/rafal.pem ubuntu@52.1.82.203 -p 2225 'rm /app/spoto/spoto ; unzip /app/spoto/spoto.zip -d /app/spoto ; sudo supervisorctl restart spoto'
+tar -cjvf spoto.tar.bz2 spoto
+scp -i /Users/rafal/dev/aws/rafal.pem -P 2225 /Users/rafal/dev/go/src/github.com/rafax/spoto/spoto.tar.bz2 ubuntu@52.1.82.203:/app/spoto
+ssh -i /Users/rafal/dev/aws/rafal.pem ubuntu@52.1.82.203 -p 2225 'cd /app/spoto; spoto ; tar -xvf /app/spoto/spoto.tar.bz2 ; sudo supervisorctl restart spoto'
