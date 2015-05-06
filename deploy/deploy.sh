c01d3600c61c5988c30a8ea6fe9d32e566247e06
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2015-05-06 21:43:34 +0200

    Switch to supervisord

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 GOOS=linux GOARCH=amd64 go build
 zip spoto.zip spoto
 scp -i /Users/rafal/dev/aws/rafal.pem -P 2225 /Users/rafal/dev/go/src/github.com/rafax/spoto/spoto.zip ubuntu@52.1.82.203:/app/spoto
-ssh -i /Users/rafal/dev/aws/rafal.pem ubuntu@52.1.82.203 -p 2225 'rm /app/spoto/spoto ; unzip /app/spoto/spoto.zip -d /app/spoto ; killall spoto ; SPOTO_DB_HOST=spoto.c0rawtfsbsk2.us-east-1.rds.amazonaws.com nohup /app/spoto/spoto | logger 2>&1 &'
+ssh -i /Users/rafal/dev/aws/rafal.pem ubuntu@52.1.82.203 -p 2225 'rm /app/spoto/spoto ; unzip /app/spoto/spoto.zip -d /app/spoto ; sudo supervisorctl restart spoto'
